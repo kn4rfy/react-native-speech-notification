5c1fe27b1dc5e62880e298659f72b473c0bcddc3
kn4rfy <kn4rfy@gmail.com>
2016-07-08 07:43:26 +0800

    Android test

--- android/src/main/java/co/ulimit/speechnotification/RCTSpeechNotification.java
+++ android/src/main/java/co/ulimit/speechnotification/RCTSpeechNotification.java
@@ -1,4 +1,29 @@
 package co.ulimit.speechnotification;
 
-public class RCTSpeechNotification {
+import android.app.Activity;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.views.view.ReactViewManager;
+import com.facebook.react.uimanager.ViewManager;
+
+/**
+ * Created by kn4rfy on 07/07/2016.
+ */
+
+public class RCTSpeechNotification implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<NativeModule>();
+    modules.add(new RCTSpeechNotificationModule(reactContext));
+    return modules;
+  }
 }
